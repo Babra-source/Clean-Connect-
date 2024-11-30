bcf668826649aa51c05603dbb4b042f97d1e967a
Beatrice Abraham <beatriceabrahamab@gmail.com>
2024-11-30 18:06:20 +0000

    upload

---
+++ view/homepage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clean Connect | Homepage</title>
+    <link rel="stylesheet" href="../assets/css/homepage.css"> 
+</head>
+<body  id = "homepage-background" style="background-image: url('../assets/images/homepage.jpg');">
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="view/login.php" class="nav-link">Sign In</a></li>
+                <li><a href="view/About.php" class="nav-link">About Page</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <div id="Homepage-container">
+    <p id="welcome-message">Professional Cleaning for your homes 
+        CleanConnect is here to provide you with well trusted and able cleaners to get
+        Your homes to sparkle .
+       
+    </p>
+
+    <p id = "welcome-message">  We clean You Relax.</p>
+    </div>
+
+
+
+
+
+    <button id = "Get-started">Get Started </button>
+
+</body>
+</html>
