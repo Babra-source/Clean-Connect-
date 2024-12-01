1ab264ec5edc0fb4b4212885f0d4dfd19a47db01
Beatrice Abraham <beatriceabrahamab@gmail.com>
2024-12-01 19:13:15 +0000

    Edited homepage

--- view/homepage.php
+++ view/homepage.php
@@ -5,34 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clean Connect | Homepage</title>
     <link rel="stylesheet" href="../assets/css/homepage.css"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
-<body  id = "homepage-background" style="background-image: url('../assets/images/homepage.jpg');">
+<body id="homepage-background" style="background-image: url('../assets/images/homepage.jpg');">
 
-    <header>
-        <nav>
-            <ul>
-                <li><a href="view/login.php" class="nav-link">Sign In</a></li>
-                <li><a href="view/About.php" class="nav-link">About Page</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <ul>
+            <li><a href="view/" class="nav-link">Homepage</a></li>
+            <li><a href="view/" class="nav-link">Be A Cleaner</a></li>
+            <li><a href="view/login.php" class="nav-link">Sign In</a></li>
+            <li><a href="view/signup.php" class="nav-link">Sign Up</a></li>
+            <li><a href="view/About.php" class="nav-link">About Page</a></li>
+        </ul>
+    </nav>
+    <hr class="full-width-line">
+</header>
 
+<div id="Homepage-container">
+    <p id="welcome-message">Professional Cleaning for your homes. CleanConnect is here to provide you with well-trusted and able cleaners to get your homes to sparkle.</p>
+    <p id="welcome-message">We clean, you relax.</p>
+    <button id="Get-started">Get Started</button>
+</div>
 
-    <div id="Homepage-container">
-    <p id="welcome-message">Professional Cleaning for your homes 
-        CleanConnect is here to provide you with well trusted and able cleaners to get
-        Your homes to sparkle .
-       
-    </p>
-
-    <p id = "welcome-message">  We clean You Relax.</p>
+<div class="cards-container">
+    <div class="card">
+        <h3>Easy Booking</h3>
+        <p>Schedule cleaning at your convenience. Our easy booking system lets you select the date and time that works best for you.</p>
     </div>
+    <div class="card">
+        <h3>Trusted Cleaners</h3>
+        <p>We vet all of our cleaners to ensure they are professional, reliable, and trustworthy.</p>
+    </div>
+    <div class="card">
+        <h3>Affordable Rates</h3>
+        <p>We offer competitive pricing without compromising on quality. Enjoy clean homes without breaking the bank.</p>
+    </div>
+</div>
+
+<div class="circle-images-container">
+    <div class="circle-image">
+        <img src="../assets/images/homepage.jpg" alt="Mission 1">
+        <p>Mission 1</p>
+    </div>
+    <div class="circle-image">
+        <img src="../assets/images/homepage.jpg" alt="Mission 2">
+        <p>Mission 2</p>
+    </div>
+    <div class="circle-image">
+        <img src="../assets/images/homepage.jpg" alt="Mission 3">
+        <p>Mission 3ggggggggggggggggggggggggggggggggggggggggg</p>
+    </div>
+</div>
 
 
 
 
-
-    <button id = "Get-started">Get Started </button>
 
 </body>
 </html>
